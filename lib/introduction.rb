59708f64dbf727897f240029df0c623ec35ebf45
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-03 20:52:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,9 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
+<<<<<<< HEAD
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+=======
+  puts "Hi, my name is #{name} and I am "
+>>>>>>> 6204b3fae215d9db38fc72ef3e7ce7d18b673686
